b672f03c7d52b9f67d2816211e2f216620d7ef30
Administrator <Administrator@USER-20181215GO>
2019-04-10 16:04:05 +0800

    t

--- src/main/java/com/zh/kj/demo/web/controller/DemoController.java
+++ src/main/java/com/zh/kj/demo/web/controller/DemoController.java
@@ -1,5 +1,15 @@
 package com.zh.kj.demo.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
 
+	@RequestMapping("/index")
+	@ResponseBody
+	public String index() {
+		return "hello world";
+	}
 }
